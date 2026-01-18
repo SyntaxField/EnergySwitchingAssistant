abdc12a39c03a6c4995a8c0f719d76a5f7b576f9
SyntaxField <x23398957@student.ncirl.ie>
2026-01-18 15:32:21 +0000

    Initial commit

---
+++ src/ui/section1/calculate/inputs/S1Form1Input.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ui.section1.calculate.inputs;
+
+/**
+ *
+ * @author jamesmurphy
+ */
+public class S1Form1Input {
+    
+    public static final int TARIFF_DAY_NIGHT = 1;
+    public static final int TARIFF_FLAT = 2;
+    
+    
+    
+}
